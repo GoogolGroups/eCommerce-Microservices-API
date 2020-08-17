916f4ba0a2a80af453152b901e10fa92874a6149
Jamie Corkhill <jamie.corkhill@live.com>
2020-08-16 20:03:23 -0500

    Add current user query logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there');
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null })
+    }
+
+    try {
+        // Get secret from env var with Kubernetes
+        const payload = jwt.verify(req.session.jwt, 'asdf');
+        return res.send({ currentUser: payload })
+    } catch (err) {
+        return res.send({ currentUser: null })
+    }
 });
 
 export { router as currentUserRouter };
