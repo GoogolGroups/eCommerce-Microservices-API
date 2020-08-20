1c9bfd2cc4a209a618f414aa48b3c881f14b58a4
Jamie Corkhill <jamie.corkhill@live.com>
2020-08-20 16:37:25 -0500

    Move to common module

---
+++ common/src/middleware/current-user.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface IUserPayload {
+    email: string;
+    password: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: IUserPayload;
+        }
+    }
+}
+
+export const currentUser = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.session?.jwt) {
+        return next();
+    }
+
+    try {
+        // TODO: Needs to come from Kubernetes env var (secret)
+        const payload = jwt.verify(req.session.jwt, 'asdf') as IUserPayload;
+        req.currentUser = payload;
+    } catch (e) {}
+
+    next();
+}
