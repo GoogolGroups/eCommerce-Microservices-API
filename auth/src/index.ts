80d0d44cdfd0b5d341002925ac43ca31152691cc
Jamie Corkhill <jamie.corkhill@live.com>
2020-08-21 17:42:01 -0500

    Update common/add tickets service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,8 +2,12 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined')
+    }
+
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
